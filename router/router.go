3c50b8b869944c75e5e7a72acd65c76d5142adb4
asaldivar <augustosaldivar@assertsolutions.com>
2025-06-10 15:29:46 -0300

    adds 405 status

--- router/router.go
+++ router/router.go
@@ -6,10 +6,18 @@ import (
 	"os"
 	"reflect"
 	"runtime"
+	"strings"
 
 	"example.com/auth"
 	"example.com/entity"
 )
+
+type Route struct {
+	hndlr    http.HandlerFunc
+	authRole string // *string
+}
+
+type RouteMap map[string] /*http.Method*/ Route
 
 var Mux *http.ServeMux
 
@@ -28,6 +36,27 @@ func corsHandler(rw http.ResponseWriter, req *http.Request) {
 	rw.WriteHeader(200)
 }
 
+func GlobalMiddleware(rm RouteMap) http.HandlerFunc {
+	fmt.Println(rm)
+	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		valor, existe := rm[req.Method]
+		if existe {
+			if valor.authRole == "" /*nil*/ {
+				corsMiddleware(printNextFuncName(valor.hndlr)).ServeHTTP(rw, req)
+			} else {
+				corsMiddleware(auth.Middleware(printNextFuncName(valor.hndlr), valor.authRole)).ServeHTTP(rw, req)
+			}
+		} else {
+			var allowedMethods []string = []string{}
+			for key := range rm {
+				allowedMethods = append(allowedMethods, key)
+			}
+			rw.Header().Set("Allow", strings.Join(allowedMethods, ","))
+			rw.WriteHeader(http.StatusMethodNotAllowed)
+		}
+	})
+}
+
 func init() {
 	var corsOrigin = os.Getenv("CORS_ORIGIN")
 	if corsOrigin != "" {
@@ -41,16 +70,35 @@ func init() {
 		}
 	}
 	var contextPath string
-	contextPath = os.Getenv("CONTEXT_PATH")
+	contextPath = os.Getenv("CONTEXT_PATH") // standarizar
 	if contextPath == "" {
 		contextPath = "/backend"
 	}
 	Mux = http.NewServeMux()
+	//Mux.HandleFunc("OPTIONS /", corsMiddleware(printNextFuncName(corsHandler)))
 	Mux.HandleFunc("/", corsMiddleware(printNextFuncName(http.NotFound)))
-	Mux.HandleFunc("OPTIONS /", corsMiddleware(printNextFuncName(corsHandler)))
-	Mux.HandleFunc("GET "+contextPath+"/entities", corsMiddleware(auth.Middleware(printNextFuncName(entity.GetEntities), "entity-read")))
-	Mux.HandleFunc("GET "+contextPath+"/entities/{id}", corsMiddleware(auth.Middleware(printNextFuncName(entity.GetEntity), "entity-read")))
-	Mux.HandleFunc("POST "+contextPath+"/entities", corsMiddleware(auth.Middleware(printNextFuncName(entity.AddEntity), "entity-create")))
-	Mux.HandleFunc("DELETE "+contextPath+"/entities/{id}", corsMiddleware(auth.Middleware(printNextFuncName(entity.RemoveEntity), "entity-delete")))
-	Mux.HandleFunc("PUT "+contextPath+"/entities/{id}", corsMiddleware(auth.Middleware(printNextFuncName(entity.UpdateEntity), "entity-update")))
+	Mux.HandleFunc(contextPath+"/entities", GlobalMiddleware(RouteMap{
+		http.MethodGet: Route{
+			hndlr:    entity.GetEntities,
+			authRole: "entity-read",
+		},
+		http.MethodPost: Route{
+			hndlr:    entity.AddEntity,
+			authRole: "entity-create",
+		},
+	}))
+	Mux.HandleFunc(contextPath+"/entities/{id}", GlobalMiddleware(RouteMap{
+		http.MethodGet: Route{
+			hndlr:    entity.GetEntity,
+			authRole: "entity-read",
+		},
+		http.MethodDelete: Route{
+			hndlr:    entity.RemoveEntity,
+			authRole: "entity-delete",
+		},
+		http.MethodPut: Route{
+			hndlr:    entity.UpdateEntity,
+			authRole: "entity-update",
+		},
+	}))
 }
